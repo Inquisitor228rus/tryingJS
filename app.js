e7c23c7f2db227a9ec35727885850c915ac0c00a
Inquisitor228rus <blooodsasha@hotmail.com>
2021-11-09 18:02:26 +0400

    вынос стилей
    теперь тут стало чуть проще ориентироваться

--- app.js
+++ app.js
@@ -8,18 +8,8 @@ const ticketNumM = document.getElementById('ticketNumM')    // элемент п
 const workHourse = 480              // часы работы в минутах
 
 // функция оперирования элементов
-function addStylesTo(node, text, color = 'black', fontSize) { 
+function changedText(node, text, color = 'black', fontSize) { 
     node.textContent = text
-    node.style.color = color
-    node.style.textAlign = 'left'
-    node.style.backgroundColor = undefined
-    node.style.padding = undefined
-    node.style.display = 'block'
-    node.style.width = '100%'
-
-    if (fontSize) {
-        node.style.fontSize = fontSize
-    }
 }
 // оперирование элементами
 btnReset.addEventListener('click',() => {
@@ -36,12 +26,12 @@ btnReset.addEventListener('click',() => {
         timeHourse = Math.trunc((workHourse - (ticketNumH.value * 60 + timeMinutes)) / 60)
     }
     // текст над полем ввода
-    addStylesTo(test, `Осталось: ${(timeHourse)} часов и ${timeMinutes} минут.`)
+    changedText(test, `Осталось: ${(timeHourse)} часов и ${timeMinutes} минут.`)
 })
 btnRun.addEventListener('click',() => {
     ticketNumH.value = 0
     ticketNumM.value = 0
     timeHourse = 8
     timeMinutes = 0
-    addStylesTo(test, `Осталось: ${(timeHourse)} часов и ${timeMinutes} минут.`)
+    changedText(test, `Осталось: ${(timeHourse)} часов и ${timeMinutes} минут.`)
 })
